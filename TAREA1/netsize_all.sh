ab9d922680442a06006f05839724f7bbe7453ad4
SebastianNeto2000 <94909648+SebastianNeto2000@users.noreply.github.com>
2021-12-09 00:50:42 -0500

    Update netsize_all.sh
    codigo para el deber 2

--- TAREA1/netsize_all.sh
+++ TAREA1/netsize_all.sh
@@ -1,1 +1,3 @@
 # Tarea del ejercicio 1.10.3 parte 2
+# Tras abrir Saavedra2013 ejecutar el siguiente código en git bush
+for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done
