5838a5932d05393d24d572f8471ebe3d8becb811
SebastianNeto2000 <sebastian.neto@est.ikiam.edu.ec>
2021-12-08 23:24:02 -0500

    se realizo el primer cambio

--- TAREA1/netsize.sh
+++ TAREA1/netsize.sh
@@ -1,1 +1,7 @@
+# Primero se debe abrir la ruta (directorio) donde esté ubicada la carpeta Saavedra2013 en su computador, y después:
+cd Saavedra2013
+# Posteriormente ejecutar el siguiente código
+cat n1.txt
+# Finalmente se obtiene el número de filas y columnas con el siguiente codigo
+echo "El ńumero de columnas es:" ; head -n1 n1.txt | grep -o " " | wc -l ; echo "El numero de filas es:" ; wc -l n1.txt; echo "Fin de la ejecucion"
 
