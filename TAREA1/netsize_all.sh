73902807652fea988ff78acea96f50e0d15cc166
SebastianNeto2000 <94909648+SebastianNeto2000@users.noreply.github.com>
2021-12-09 01:16:21 -0500

    Se aumentó un paso

--- TAREA1/netsize_all.sh
+++ TAREA1/netsize_all.sh
@@ -1,3 +1,5 @@
 # Tarea del ejercicio 1.10.3 parte 2
-# Tras abrir Saavedra2013 ejecutar el siguiente código en git bush
+# Abrir el directoria del archivo Saavadra2013
+cd Saavedra2013
+# Finalmente ejecutar el siguiente código en git bush
 for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done
