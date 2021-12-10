171fe23c7ca75a904447fe2d6a7a057f29330032
SebastianNeto2000 <94909648+SebastianNeto2000@users.noreply.github.com>
2021-12-09 22:57:09 -0500

    Se añadió información del resultado

--- TAREA1/netsize_all.sh
+++ TAREA1/netsize_all.sh
@@ -1,5 +1,7 @@
 # Tarea del ejercicio 1.10.3 parte 2
-# Abrir el directoria del archivo Saavadra2013
+# Abrir el directoria del archivo Saavedra2013
+# Correr el código:
 cd Saavedra2013
 # Finalmente ejecutar el siguiente código en git bush
 for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done
+# Se obtienen todos los valores de fila y columna para los 59 archivos .txt
